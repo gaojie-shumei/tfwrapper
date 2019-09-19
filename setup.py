4ae26a30c5524c725e2a1868ea7c054e7a2c626a
gaojie-202 <gaojie-202@gaojie-202>
2019-09-19 19:39:46 +0800

    set a gpu version for tensorflow-gpu

--- setup.py
+++ setup.py
@@ -4,8 +4,32 @@ Created on 2019年9月17日
 @author: gaojie-202
 '''
 import setuptools
+# setuptools.setup(
+#     name = "tfwrapper",
+#     version = "1.0",
+#     description = "This is a wrapper for tf.Session().run and tf.data.Dataset in tensorflow1.x",
+#     author = "Jie Gao",
+#     maintainer = "Jie Gao",
+#     author_email = "gaojiexcq@163.com",
+#     maintainer_email = "gaojiexcq@163.com",
+#     url = "https://github.com/gjxcq/tfwrapper",
+#     packages = setuptools.find_packages(exclude=("extendNet")),
+#     platforms = "python3",
+#     download_url = "https://github.com/gjxcq/tfwrapper",
+#     install_requires = [
+#         "tensorflow<=1.13.1,>=1.10",
+#         "gensim<=3.6.0",
+#         "numpy<=1.16.4"
+#     ],
+#     requires = [
+#         "tensorflow",
+#         "gensim",
+#         "numpy"
+#     ],
+#     license = "MIT"
+# )
 setuptools.setup(
-    name = "tfwrapper",
+    name = "tfwrapper-gpu",
     version = "1.0",
     description = "This is a wrapper for tf.Session().run and tf.data.Dataset in tensorflow1.x",
     author = "Jie Gao",
@@ -17,13 +41,14 @@ setuptools.setup(
     platforms = "python3",
     download_url = "https://github.com/gjxcq/tfwrapper",
     install_requires = [
-        "tensorflow==1.13.1",
-        "gensim==3.6.0",
-        "numpy==1.16.4"
+        "tensorflow-gpu<=1.13.1,>=1.10",
+        "gensim<=3.6.0",
+        "numpy<=1.16.4"
     ],
     requires = [
         "tensorflow",
         "gensim",
         "numpy"
-    ]
+    ],
+    license = "MIT"
 )
