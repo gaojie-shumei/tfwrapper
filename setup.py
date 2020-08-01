730723c466794124b4489f48810d484abeba0717
gaojie-shumei <1457414722@qq.com>
2020-08-01 11:02:45 +0800

    setup description change

--- setup.py
+++ setup.py
@@ -4,18 +4,23 @@ Created on 2019年9月17日
 @author: gaojie-202
 '''
 import setuptools
+description = '''
+    This is a wrapper for tensorflow and a word2vecUtil with gensim, 
+    this version rewrite the datawrapper for general and set it in basev2 package,
+    or can use it with shuffix 'V2'
+    '''
 setuptools.setup(
     name = "tfwrapper",
-    version = "2.0.3",
-    description = "This is a wrapper for tensorflow, the word2vecUtil add method window_format, change datawrapper uncertain length data read",
+    version = "2.1.0",
+    description = description,
     author = "Jie Gao",
     maintainer = "Jie Gao",
     author_email = "gaojiexcq@163.com",
     maintainer_email = "gaojiexcq@163.com",
-    url = "https://github.com/gjxcq/tfwrapper",
+    url = "https://github.com/gaojie-shumei/tfwrapper",
     packages = setuptools.find_packages(exclude=("extendNet")),
     platforms = "python3",
-    download_url = "https://github.com/gjxcq/tfwrapper",
+    download_url = "https://github.com/gaojie-shumei/tfwrapper",
     install_requires = [
         "tensorflow>=1.12",
         "gensim<=3.6.0",
@@ -30,16 +35,16 @@ setuptools.setup(
 )
 # setuptools.setup(
 #     name = "tfwrapper-gpu",
-#     version = "2.0.3",
-#     description = "This is a wrapper for tensorflow, the word2vecUtil add method window_format, change datawrapper uncertain length data read",
+#     version = "2.1.0",
+#     description = description,
 #     author = "Jie Gao",
 #     maintainer = "Jie Gao",
 #     author_email = "gaojiexcq@163.com",
 #     maintainer_email = "gaojiexcq@163.com",
-#     url = "https://github.com/gjxcq/tfwrapper",
+#     url = "https://github.com/gaojie-shumei/tfwrapper",
 #     packages = setuptools.find_packages(exclude=("extendNet")),
 #     platforms = "python3",
-#     download_url = "https://github.com/gjxcq/tfwrapper",
+#     download_url = "https://github.com/gaojie-shumei/tfwrapper",
 #     install_requires = [
 #         "tensorflow-gpu>=1.12",
 #         "gensim<=3.6.0",
